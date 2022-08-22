fe6b358899a1d1183f8351254e77cc4e70529e2d
Dustin Shaddawvine <104474633+Shaddawvine@users.noreply.github.com>
2022-08-21 18:45:22 -0600

    Add files via upload

---
+++ jeep-sales/src/main/java/com/promineotech/jeep/entity/JeepModel.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.promineotech.jeep.entity;
+
+/**
+ * @author shadd
+ *
+ */
+public enum JeepModel {
+  GRAND_CHEROKEE, WRANGLER, CHEROKEE, COMPASS, RENEGADE, GLADIATOR, WRANGLER_4XE
+}
